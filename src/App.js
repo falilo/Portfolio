64386ab3ad7099c50ab263341bd5e42fc8c955d3
abdel <aboualyet63@gmail.com>
2020-12-08 08:21:14 -0500

    starting off my portfolio project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+ import example from './examples/example.gif';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+    
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
