64d6903a7ea473b39b55e8652666bc9671120236
abdel <aboualyet63@gmail.com>
2020-12-12 09:14:49 -0500

    add connected my resume to resume.js and modified contact.js

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,12 +1,81 @@
 import React ,{Component} from 'react';
+import '../App.css';
 
 
 class Contact extends Component{
+
+      constructor(props){
+          super(props);
+          this.state ={
+                       Name:"",
+                       phone: "",
+                       email: ""
+                      }
+
+      }
+
+      handleNameChange = e => {
+        this.setState({ Name: e.target.value });
+       // console.log(e.target.value)
+        const fm = e.target.value
+       console.log('/////////////',fm);
+    //    alert(`hey ${fm} your message was successful sent`);
+         
+       
+      };
+    
+      handleEmail = e => {
+        this.setState({ email: e.target.value});
+      };
+    
+      handleContact = e => {
+        this.setState({ Contact: e.target.value});
+       
+      };
+
+      onFormSubmit = (event) => {
+        // to prevent page reload on form submit
+        event.preventDefault();
+        console.log(this.state)
+    
+        this.setState({Name:''})
+       
+        alert(`hey ${this.Name} your message was successful sent`);
+        
+        
+
+       
+
+    }
+      
+
+    
     render(){
         return(
-            <div>
-                <h1>Contact page </h1>
-            </div>
+            <form className ='register_components'>
+                    <div>
+                        <label>
+                            Name :
+                            <input type="text" name="name" onChange={this.handleNameChange} value={this.state.Name}/>
+                        </label>   
+                    </div>
+                    <br/>
+                    <div>
+                        <label>
+                            Phone :
+                            <input type="text" name="name" />
+                         </label>
+                    </div>
+                    <br/>
+                    <div>
+                        <label>
+                            Email :
+                            <input type="text"  email ='email'/>
+                        </label>
+                    </div>
+                    <button style={{background:'blue', borderRadius:'12px'}} onClick = {this.onFormSubmit}> Submit</button>
+
+            </form>
         )
     }
 }
